30be0378581ca91d1051a4f123e97ec0936641ab
rhtaylor <randyisbusy@gmail.com>
2019-11-08 11:00:28 -0800

    updated dotenv to production

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ ENV['SINATRA_ENV'] ||= "production"
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
-Dotenv.load if ENV['SINATRA_ENV'] == "development"
+Dotenv.load if ENV['SINATRA_ENV'] == "production"
 
 set :database_file, "./database.yml"
 
